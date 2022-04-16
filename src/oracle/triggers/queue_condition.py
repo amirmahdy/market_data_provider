dd2d599762254c4d48f0b358f5b4bae4f0ffb8ea
hatef madani <hatefmdn@gmail.com>
2022-04-16 07:58:35 +0430

    Replace queue condition trigger print with websocket broadcast

--- src/oracle/triggers/queue_condition.py
+++ src/oracle/triggers/queue_condition.py
@@ -1,11 +1,14 @@
 def check_instrument_queue_status(isin):
     from oracle.data_type.instrument_market_data import InstrumentData
     from oracle.services.tsetmc_market import get_tse_instrument_data
+    from morpheus.services.broadcast import broadcast_trigger
 
     market_data = InstrumentData.get(ref_group='market', isin=isin)
     if market_data == None:
         market_data = get_tse_instrument_data(isin)
     if (market_data['high_allowed_price'] == market_data['bid_ask_first_row']['best_buy_price']):
-        print('buy queue condition')
+        broadcast_trigger(
+            data={'trigger_type': 'queue_condition', 'isin': isin, 'queue_side': 'buy'})
     if market_data['low_allowed_price'] == market_data['bid_ask_first_row']['best_sell_price']:
-        print('sell queue condition')
+        broadcast_trigger(
+            data={'trigger_type': 'queue_condition', 'isin': isin, 'queue_side': 'buy'})
