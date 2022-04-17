29ff3e42868261e498fb28ebcad214acd39f14cc
amirmahdy <amir.malayjerdi@gmail.com>
2022-04-17 12:49:55 +0430

    Add Parand ISIN Online+ start

--- src/oracle/apps.py
+++ src/oracle/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import sys
 
 
 class OracleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'oracle'
+
+    def ready(self):
+        from .services.online_plus import LS_Class
+        if "worker" not in sys.argv:
+            return True
+        LS_Class()
