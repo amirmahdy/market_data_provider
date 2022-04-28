311b6d4b1bb69f89f53597bf9fcaa629cb9826c4
amirmahdy <amir.malayjerdi@gmail.com>
2022-04-28 18:59:37 +0430

    Change in DATA dir

--- src/mdp/settings.py
+++ src/mdp/settings.py
@@ -128,11 +128,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-DATA_ROOT = os.path.join(BASE_DIR, 'data')
-DATA_URL = '/data/'
+DATA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'data')
+DATA_URL = 'data/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
