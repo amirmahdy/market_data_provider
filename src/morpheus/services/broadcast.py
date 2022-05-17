7584548d10357c51e409fc476c27686aad38200e
amirmahdy <amir.malayjerdi@gmail.com>
2022-05-17 10:54:18 +0430

    Bug fix

--- src/morpheus/services/broadcast.py
+++ src/morpheus/services/broadcast.py
@@ -5,19 +5,19 @@ from channels.layers import get_channel_layer
 def broadcast_trigger(isin: str, data: dict):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(f"trigger_{isin}",
-                                            {"type": "send_data", "msg_type": "trigger", "data": data}, )
+                                            {"type": "send_data", "msg_type": "trigger", "isin": isin, "data": data}, )
 
 
 def broadcast_market_data(isin: str, market_data):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(f"market_{isin}", {
-        "type": "send_data", "msg_type": "market_data", "data": market_data})
+        "type": "send_data", "msg_type": "market_data", "isin": isin, "data": market_data})
 
 
 def broadcast_askbid_data(isin: str, askbid_data):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(f"askbid_{isin}", {
-        "type": "send_data", "msg_type": "askbid_data", "data": askbid_data})
+        "type": "send_data", "msg_type": "askbid_data", "isin": isin, "data": askbid_data})
 
 
 def broadcast_indices_data(index_data):
@@ -29,4 +29,4 @@ def broadcast_indices_data(index_data):
 def broadcast_indinst_data(isin: str, indinst_data):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(f"indinst_{isin}", {
-        "type": "send_data", "msg_type": "indinst_data", "data": indinst_data})
+        "type": "send_data", "msg_type": "indinst_data", "isin": isin, "data": indinst_data})
