f1728a6c41ccfb4e0273a555f1fef20ca9ae5389
amirmahdy <amir.malayjerdi@gmail.com>
2022-06-11 12:04:40 +0430

    bug fix

--- src/oracle/tasks.py
+++ src/oracle/tasks.py
@@ -30,7 +30,7 @@ def market_data_update():
             if res['market_status'] != instrument.market_status:
                 instrument.market_status = res['market_status']
                 instrument.save()
-                instrument_state = {'state': res['market_status'], 'stateCode' : translate_state(res['market_status'])}
+                instrument_state = {'state': res['market_status'], 'state_code': translate_state(res['market_status'])}
                 InstrumentData.update(instrument.isin, 'state', instrument_state)
                 broadcast_trigger(isin=instrument.isin, data={'trigger_type': 'instrument_market_status_change'})
                 broadcast_market_data(isin=instrument.isin,
