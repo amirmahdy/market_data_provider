4d0347a3b0e7ad17fe5f99d9543447d65c815fe9
amirmahdy <amir.malayjerdi@gmail.com>
2022-06-18 10:05:34 +0430

    bug fix

--- src/oracle/apps.py
+++ src/oracle/apps.py
@@ -8,10 +8,10 @@ class OracleConfig(AppConfig):
 
     def ready(self):
         from .services.online_plus import LS_Class
-        from oracle.services.base import initial_setup
+        # from oracle.services.base import initial_setup
 
         if "worker" not in sys.argv:
             return True
 
-        initial_setup()
+        # initial_setup()
         LS_Class()
