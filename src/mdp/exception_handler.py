f29d0f42fd405cb66b57e90b8a8d11c3df46e97c
amirmahdy <amir.malayjerdi@gmail.com>
2022-07-05 12:50:14 +0430

    Bug fix

--- src/mdp/exception_handler.py
+++ src/mdp/exception_handler.py
@@ -5,16 +5,23 @@ from mdp.log import Log
 log = Log()
 
 
-def exception_handler(log_type):
+def unpredicted_exception_handler(log_type):
     def decorator(func):
         def inner(*args, **kwargs):
             try:
                 func(*args, **kwargs)
             except Exception:
                 _, value, traceback = sys.exc_info()
-                log('TYPE %s \nFILE %s \nFUNC %s \nLINE %s \nERRR %s \nINPT %s' % (log_type, inspect.getfile(
+                log('TYPE: %s \nFILE: %s \nFUNC: %s \nLINE: %s \nERRR: %s \nINPT: %s' % (log_type, inspect.getfile(
                     func), func.__name__, str(traceback.tb_next.tb_lineno), str(value), str(args) + str(kwargs)))
 
         return inner
 
     return decorator
+
+
+def exception_handler(log_type, frame):
+    frames = inspect.getframeinfo(frame)
+    _, value, traceback = sys.exc_info()
+    log('TYPE: %s \nFILE: %s \nFUNC: %s \nLINE: %s \nERRR: %s ' % (
+        log_type, frames.function, frames.filename, str(traceback.tb_lineno), str(value)))
--- src/oracle/tasks.py
+++ src/oracle/tasks.py
@@ -13,93 +13,100 @@ from oracle.services.tsetmc_askbid import get_askbid_history
 from datetime import datetime, timedelta
 from oracle.triggers.queue_condition import broadcast_instrument_queue_status
 from morpheus.services.broadcast import broadcast_trigger, broadcast_market_data
-from mdp.log import Log
-from mdp.exception_handler import exception_handler
-
-log = Log()
+from mdp.exception_handler import unpredicted_exception_handler, exception_handler
+import inspect
 
 cache = Cache()
 env = environ.Env()
 
 
 @shared_task(name="tsetmc_market_data_update")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def market_data_update():
     instruments = Instrument.get_instruments()
     for instrument in instruments:
-        res = get_tse_instrument_data(instrument)
-        InstrumentData.update(instrument.isin, "market", res)
+        try:
+            res = get_tse_instrument_data(instrument)
+            InstrumentData.update(instrument.isin, "market", res)
 
-        # update market status
-        if instrument.market_status is not None and ['market_status'] != instrument.market_status:
-            instrument.market_status = res['market_status']
-            instrument.save()
-            instrument_state = {'state': res['market_status']}
-            InstrumentData.update(instrument.isin, 'state', instrument_state)
-            broadcast_trigger(isin=instrument.isin, data={'trigger_type': 'instrument_market_status_change'})
-            broadcast_market_data(isin=instrument.isin,
-                                  market_data=InstrumentData.get(isin=instrument.isin, ref_group='market'))
-
+            # update market status
+            if instrument.market_status is not None and ['market_status'] != instrument.market_status:
+                instrument.market_status = res['market_status']
+                instrument.save()
+                instrument_state = {'state': res['market_status']}
+                InstrumentData.update(instrument.isin, 'state', instrument_state)
+                broadcast_trigger(isin=instrument.isin, data={'trigger_type': 'instrument_market_status_change'})
+                broadcast_market_data(isin=instrument.isin,
+                                      market_data=InstrumentData.get(isin=instrument.isin, ref_group='market'))
+        except Exception:
+            exception_handler("DEBUG", inspect.currentframe())
     return True
 
 
 @shared_task(name="tsetmc_askbid_yesterday_update")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def tsetmc_askbid_yesterday_update(days):
     instruments = Instrument.get_instruments()
     for day in range(1, days + 1):
         yesterday = datetime.strftime(datetime.now() - timedelta(day), '%Y%m%d')
         path = settings.DATA_ROOT + "/askbid/"
         for instrument in instruments:
-            today_askbids = get_askbid_history(instrument.tse_id, yesterday)
-            if today_askbids:
-                sym = instrument.en_symbol.lower()
-                create_csv(path + sym + "/" + yesterday + ".csv", today_askbids,
-                           fieldnames=["time", "buy_price_1", "buy_volume_1", "buy_count_1", "sell_price_1",
-                                       "sell_volume_1", "sell_count_1", "buy_price_2", "buy_volume_2",
-                                       "buy_count_2",
-                                       "sell_price_2", "sell_volume_2", "sell_count_2", "buy_price_3",
-                                       "buy_volume_3",
-                                       "buy_count_3", "sell_price_3", "sell_volume_3", "sell_count_3",
-                                       "buy_price_4",
-                                       "buy_volume_4", "buy_count_4", "sell_price_4", "sell_volume_4",
-                                       "sell_count_4",
-                                       "buy_price_5", "buy_volume_5", "buy_count_5", "sell_price_5",
-                                       "sell_volume_5",
-                                       "sell_count_5"], frmt="w+")
-
+            try:
+                today_askbids = get_askbid_history(instrument.tse_id, yesterday)
+                if today_askbids:
+                    sym = instrument.en_symbol.lower()
+                    create_csv(path + sym + "/" + yesterday + ".csv", today_askbids,
+                               fieldnames=["time", "buy_price_1", "buy_volume_1", "buy_count_1", "sell_price_1",
+                                           "sell_volume_1", "sell_count_1", "buy_price_2", "buy_volume_2",
+                                           "buy_count_2",
+                                           "sell_price_2", "sell_volume_2", "sell_count_2", "buy_price_3",
+                                           "buy_volume_3",
+                                           "buy_count_3", "sell_price_3", "sell_volume_3", "sell_count_3",
+                                           "buy_price_4",
+                                           "buy_volume_4", "buy_count_4", "sell_price_4", "sell_volume_4",
+                                           "sell_count_4",
+                                           "buy_price_5", "buy_volume_5", "buy_count_5", "sell_price_5",
+                                           "sell_volume_5",
+                                           "sell_count_5"], frmt="w+")
+            except Exception:
+                exception_handler("DEBUG", inspect.currentframe())
     return True
 
 
 @shared_task(name="tsetmc_trade_yesterday_update")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def trade_data_yesterday_update(days):
     instruments = Instrument.get_instruments()
     for day in range(1, days + 1):
         yesterday = datetime.strftime(datetime.now() - timedelta(day), '%Y%m%d')
         path = settings.DATA_ROOT + "/trade/"
         for instrument in instruments:
-            today_trades = get_trades(instrument.tse_id, yesterday)
-            if today_trades:
-                sym = instrument.en_symbol.lower()
-                create_csv(path + sym + "/" + yesterday + ".csv", today_trades,
-                           fieldnames=['t', 'q', 'p'], frmt="w+")
-
+            try:
+                today_trades = get_trades(instrument.tse_id, yesterday)
+                if today_trades:
+                    sym = instrument.en_symbol.lower()
+                    create_csv(path + sym + "/" + yesterday + ".csv", today_trades,
+                               fieldnames=['t', 'q', 'p'], frmt="w+")
+            except Exception:
+                exception_handler("DEBUG", inspect.currentframe())
     return True
 
 
 @shared_task(name="tsetmc_trade_today_update")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def trade_data_today_update():
     instruments = Instrument.get_instruments()
     for instrument in instruments:
-        today_trades = get_trades(instrument.tse_id)
-        InstrumentData.update(instrument.isin, 'trades', today_trades)
+        try:
+            today_trades = get_trades(instrument.tse_id)
+            InstrumentData.update(instrument.isin, 'trades', today_trades)
+        except Exception:
+            exception_handler("DEBUG", inspect.currentframe())
     return True
 
 
 @shared_task(name="tsetmc_trade_kline")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def tsetmc_trade_kline(days):
     instruments = Instrument.get_instruments()
     to_date = datetime.strftime(datetime.now(), '%Y%m%d')
@@ -107,39 +114,45 @@ def tsetmc_trade_kline(days):
     path = settings.DATA_ROOT + "/equity/usa/daily/"
 
     for instrument in instruments:
-        rows = get_kline(from_date, to_date, instrument.tse_id, env("TSETMC_USERNAME"), env("TSETMC_PASSWORD"))
-        sym = instrument.en_symbol
+        try:
+            rows = get_kline(from_date, to_date, instrument.tse_id, env("TSETMC_USERNAME"), env("TSETMC_PASSWORD"))
+            sym = instrument.en_symbol
 
-        if fsutil.exists(path + sym.lower() + ".zip"):
-            zipfile.ZipFile(path + sym.lower() + ".zip", "r").extractall(path)
-            fsutil.remove_file(path + sym.lower() + ".zip")
+            if fsutil.exists(path + sym.lower() + ".zip"):
+                zipfile.ZipFile(path + sym.lower() + ".zip", "r").extractall(path)
+                fsutil.remove_file(path + sym.lower() + ".zip")
 
-        create_csv(path + sym.upper() + ".csv", rows, fieldnames=None, frmt="a")
-        zipfile.ZipFile(path + sym.lower() + ".zip", "w", zipfile.ZIP_DEFLATED). \
-            write(path + sym.upper() + ".csv", arcname=sym.upper() + ".csv")
-        fsutil.remove_file(path + sym.upper() + ".csv")
+            create_csv(path + sym.upper() + ".csv", rows, fieldnames=None, frmt="a")
+            zipfile.ZipFile(path + sym.lower() + ".zip", "w", zipfile.ZIP_DEFLATED). \
+                write(path + sym.upper() + ".csv", arcname=sym.upper() + ".csv")
+            fsutil.remove_file(path + sym.upper() + ".csv")
+        except Exception:
+            exception_handler("DEBUG", inspect.currentframe())
 
     return True
 
 
 @shared_task(name="trade_tick_data")
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def trade_tick_data():
     instruments = Instrument.get_instruments()
     cur_date = datetime.strftime(datetime.now() - timedelta(1), '%Y%m%d')
     for instrument in instruments:
-        rows = get_tick_data(instrument.tse_id, cur_date)
-        if rows:
-            sym = instrument.en_symbol
-            path = settings.DATA_ROOT + "/equity/usa/tick/" + sym.upper() + "/"
-            zip_filename = cur_date + "_trade.zip"
-            csv_filename = cur_date + "_trade.csv"
-            create_zip_on_csv(path, zip_filename, csv_filename, rows)
+        try:
+            rows = get_tick_data(instrument.tse_id, cur_date)
+            if rows:
+                sym = instrument.en_symbol
+                path = settings.DATA_ROOT + "/equity/usa/tick/" + sym.upper() + "/"
+                zip_filename = cur_date + "_trade.zip"
+                csv_filename = cur_date + "_trade.csv"
+                create_zip_on_csv(path, zip_filename, csv_filename, rows)
+        except Exception:
+            exception_handler("DEBUG", inspect.currentframe())
     return True
 
 
 @shared_task(name='online_plus_socket_renew')
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def online_plus_socket_renew():
     from oracle.services.online_plus import LS_Class
     ls = LS_Class()
@@ -148,9 +161,12 @@ def online_plus_socket_renew():
 
 
 @shared_task(name='check_queue_condition')
-@exception_handler("DEBUG")
+@unpredicted_exception_handler("DEBUG")
 def check_queue_condition():
     instruments = Instrument.get_instruments()
     for instrument in instruments:
-        broadcast_instrument_queue_status(instrument)
+        try:
+            broadcast_instrument_queue_status(instrument)
+        except Exception:
+            exception_handler("DEBUG", inspect.currentframe())
     return True
